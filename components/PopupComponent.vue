c799eeeba61c5e7b72cb007aea1b69819b8a9cca
vylenne <vylenne@192.168.1.5>
2022-07-17 14:53:48 +0300

    added popup and small fixrs

---
+++ components/PopupComponent.vue
@@ -0,0 +1,84 @@
+<template>
+  <vue-bottom-sheet
+    ref="popup"
+    max-width="100%"
+    max-height="45%"
+    :rounded="false"
+    :is-full-screen="true"
+    :overlay-color="overlayColorSelect"
+  >
+    <div class="py-10 container mx-auto overflow-y-scroll relative">
+      <Close
+        class="absolute top-10 right-0 z-10 cursor-pointer"
+        @click.native="close"
+      />
+      <h1 class="text-3xl font-semibold mb-5">{{ resourceCategory }}</h1>
+      <p class="w-3/4 mb-10">{{ resourceDescription }}</p>
+      <div class="grid grid-cols-2">
+        <div v-for="(item, i) in resourceLinks" :key="i" class="flex mb-5">
+          <TagComponent :type-id="item.id" :type-of-resource="item.tag" />
+          <a :href="item.link" target="_blank" class="mt-0.5">{{ item.title }}</a>
+        </div>
+      </div>
+    </div>
+  </vue-bottom-sheet>
+</template>
+
+<script>
+import VueBottomSheet from '@webzlodimir/vue-bottom-sheet'
+import Close from './svg/Close.vue'
+import TagComponent from './TagComponent.vue'
+
+export default {
+  name: 'PopupComponent',
+  components: {
+    VueBottomSheet,
+    Close,
+    TagComponent
+},
+  props: {
+    resource: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  data() {
+    return {
+      overlayColorSelect: '#6B728040',
+    }
+  },
+  computed: {
+    resourceCategory() {
+      return this.resource?.category ?? ''
+    },
+    resourceDescription() {
+      return this.resource?.description ?? ''
+    },
+    resourceLinks() {
+      return this.resource?.links ?? ''
+    },
+  },
+  methods: {
+    close() {
+      this.$refs.popup.close()
+    },
+  },
+}
+</script>
+
+<style scoped>
+.bottom-sheet >>> .bottom-sheet__pan {
+  display: none;
+}
+
+/* .bottom-sheet >>> .bottom-sheet__card,
+.bottom-sheet >>> .bottom-sheet__content {
+  height: auto !important;
+} */
+
+@media screen and (max-width: 450px) {
+  .bottom-sheet >>> .bottom-sheet__pan {
+    display: block;
+  }
+}
+</style>
--- pages/ui-ux/index.vue
+++ pages/ui-ux/index.vue
@@ -5,28 +5,43 @@
     <p class="mt-6 mb-8 w-3/4">
       {{ data.description }}
     </p>
-    <div v-for="(block, id) in data.content" :key="id">
-      <BlockOfContent :title="block.title" :data-of-block="block.content"/>
+    <div v-for="(block, id) in data.categories" :key="id">
+      <BlockOfContent
+        :title="block.title"
+        :data-of-block="block.content"
+        @open="open($event)"
+      />
     </div>
     <h2 class="text-xl font-medium my-4">
       Все в ваших руках. Нарабатывайте практику и будет вам счастье!:)
     </h2>
     <Footer />
+    <Popup ref="wrapper" :resource="resource" />
   </div>
 </template>
 
 <script>
-import BlockOfContent from '@/components/BlockOfContent/index.vue'
+import BlockOfContent from '@/components/BlockOfContent/BlockOfContent.vue'
 import BackToMain from '@/components/BackToMain.vue'
 import Footer from '@/components/FooterComponent.vue'
+import Popup from '@/components/PopupComponent.vue'
+
 import data from '@/content/pages/uiuxMain.json'
 
 export default {
   name: 'UiUxDesign',
-  components: { BlockOfContent, BackToMain, Footer },
+  components: { BlockOfContent, BackToMain, Footer, Popup },
   data() {
     return {
       data,
+      resource: null
+    }
+  },
+  methods: {
+    open(category) {
+      const resource = data.content.filter(item => item.category === category)
+      this.resource = resource[0] ?? {}
+      this.$refs.wrapper.$refs.popup.open()
     }
   },
 }
