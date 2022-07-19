616ba8ed1b627939a541a2f9284c38492dc7bca3
vylenne <vylenne@192.168.1.5>
2022-07-20 00:29:48 +0300

    remove count resources

--- components/mainBlocks/uiuxDesign.vue
+++ components/mainBlocks/uiuxDesign.vue
@@ -6,9 +6,9 @@
     <p class="text-3xl font-bold">UI/UX Design</p>
     <div class="flex justify-between items-end">
       <div>
-        <p>10 books</p>
+        <!-- <p>10 books</p>
         <p>4 articles</p>
-        <p>50 resources</p>
+        <p>50 resources</p> -->
       </div>
       <UIUXIcon class="w-1/3" />
     </div>
